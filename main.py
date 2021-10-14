5f4f3b10560791cda87bbdee7c64a4ba5ce1ea47
unknown <asef18766@gmail.com>
2021-10-14 18:12:32 +0800

    add combination craweler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from pprint import pprint
 
 from form import (
     export_to_xlsx,
@@ -8,16 +9,29 @@ from craweler import (
     get_player_id,
     get_player_matches
 )
+from datetime import datetime
 
-logging.basicConfig(level = logging.DEBUG)
+def get_currect_time()->str:
+    return datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+root_logger= logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(f'{get_currect_time()}.log', 'w', 'utf-8')
+handler.setFormatter(logging.Formatter('%(asctime)s [ %(name)s ] %(levelname)s : %(message)s'))
+root_logger.addHandler(handler)
 
 def main():
     player_data = read_csv_reply("LOL數據調查.csv")
     match_data = []
     for email, playername in player_data:
-        print(f"conducting queries on {playername}")
-        player_id =  get_player_id(playername)
-        match_data += get_player_matches(player_id)
+        try:
+            print(f"conducting queries on {playername}")
+            player_id =  get_player_id(playername)
+            match_data += get_player_matches(player_id)
+        except Exception as e:
+            logging.error(f"error on processing {playername}")
+            
     export_to_xlsx(match_data, "test.xlsx")
+    
 if __name__ == "__main__" :
     main()
